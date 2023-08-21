f1a7c964b7d4401e980a35e7c1adc3cd963b264b
GeekchanskiY <astrovskiy.04@gmail.com>
2023-08-21 16:30:30 +0300

    init

---
+++ init.lua
@@ -0,0 +1,66 @@
+local opt = vim.opt
+local g = vim.g
+
+vim.g.did_load_filetypes = 1
+vim.g.formatoptions = "qrn1"
+vim.opt.showmode = false
+vim.opt.updatetime = 100
+vim.wo.signcolumn = "yes"
+vim.opt.scrolloff = 8
+vim.opt.wrap = false
+vim.wo.linebreak = true
+vim.opt.virtualedit = "block"
+vim.opt.undofile = true
+vim.opt.shell = "/bin/zsh"
+
+-- Mouse
+vim.opt.mouse = "a"
+vim.opt.mousefocus = true
+
+-- Line Numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Shorter messages
+vim.opt.shortmess:append("c")
+
+-- Indent Settings
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+
+
+-- Fillchars
+vim.opt.fillchars = {
+    vert = "│",
+    fold = "⠀",
+    eob = " ", -- suppress ~ at EndOfBuffer
+    -- diff = "⣿", -- alternatives = ⣿ ░ ─ ╱
+    msgsep = "‾",
+    foldopen = "▾",
+    foldsep = "│",
+    foldclose = "▸"
+}
+
+-- Plugins
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.2',
+-- or                            , branch = '0.1.x',
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
+
+end)
